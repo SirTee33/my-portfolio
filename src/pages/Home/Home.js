fceebcd63d3a4b1b3964c3dfee14f492c444b4cf
SirTee <ojotaye33@gmail.com>
2025-06-19 18:56:36 +0100

    Updated my-portfolio

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -12,7 +12,7 @@ const Home = () => {
         <h2>Full Stack Software Developer</h2>
         <p>
           I specialize in building robust and scalable web applications using modern technologies like
-          <strong> React</strong>, <strong>Node.js</strong>, and <strong>MongoDB</strong>. With a passion for solving real-world problems through code, I’ve developed full-stack solutions that are efficient, user-friendly, and performance-driven. From designing interactive user interfaces to building RESTful APIs and managing databases, I bring both frontend and backend expertise to every project. Whether you need a sleek portfolio, an e-commerce platform, or a custom web solution — I can build it.
+          <strong> React</strong>, <strong>Node.js</strong>, and <strong>MongoDB</strong>. With a passion for solving real-world problems through code, I’ve developed full-stack solutions that are efficient, user-friendly, and performance-driven. From designing interactive user interfaces to building RESTful APIs and managing databases, I bring both frontend and backend expertise to every project.
         </p>
         <a href="/projects" className="btn">See More</a>
       </div>
